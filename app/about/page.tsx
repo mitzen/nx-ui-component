def6e2bb07374fb8540967d491dd427a3ce022c9
jeremy woo <jeremy>
2023-07-22 10:33:45 +1200

    added header component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,21 @@
-import Image from 'next/image'
-import Hello from '../components/hello'
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ProTip from '@/components/Protip';
 import Copyright from '@/components/Copyright';
-import Header from '../components/Header';
+import Header from '@/components/Header';
+
 
 export default function Home() {
   return (
     <Container maxWidth="sm">
-
-    <Header></Header>
+   
+   <Header></Header>
+   
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
-        Material UI - Next.js example
+        actual about
       </Typography>
-
-      <Hello></Hello>
         Go to the about page
       <ProTip />
       <Copyright />
