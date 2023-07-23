ffe4ac6075956608c7c29188aa2c4d53e3cfa637
jeremy woo <jeremy>
2023-07-23 20:50:23 +1200

    fix deployment static files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        outputStandalone: true,
-    }
+    // experimental: {
+    //     outputStandalone: true,
+    // }
+    output: 'standalone',
 }
 
 module.exports = nextConfig
-
