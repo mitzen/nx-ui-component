8c61d76d62e3a29d75f2675d117e05008f9edd4a
jeremy woo <jeremy>
2023-07-24 22:01:35 +1200

    added prisma library

--- components/containers/search/searchresult.tsx
+++ components/containers/search/searchresult.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 
 const SearchResult = () => {
