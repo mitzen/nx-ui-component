29ef28b874b954dabe7c9cf118b4b8e7992a8773
jeremy woo <jeremy>
2023-07-24 20:06:23 +1200

    uses box within a grid to align item

--- components/containers/jobs/topjobs.tsx
+++ components/containers/jobs/topjobs.tsx
@@ -9,14 +9,13 @@ const TopJobs = () => {
             spacing={0}
             alignItems="center"
             justifyContent="center"
-            sx={{ minHeight: '20vh' }}
-            >
-    
+            sx={{ minHeight: '16vh' }}
+            >    
             <Grid item xs={1}>
             </Grid>
 
             <Grid item xs={11}>
-               Top jobs based on your search
+               Top jobs based    on your search
             </Grid>
            
           
--- components/containers/search/jobsearch.tsx
+++ components/containers/search/jobsearch.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-
-const jobsearchpadding = {
-  color: 'blue'
-};
+import Box from '@mui/material/Box';
 
 const JobSearch = () => {
     return (
-    <Grid
+        <Grid
         container
         spacing={0}
         alignItems="center"
@@ -17,12 +14,17 @@ const JobSearch = () => {
         >
 
         <Grid item xs={8}>
-            <TextField id="filled-basic" label="Search" variant="filled" />
-            <TextField id="filled-basic" label="Classification" variant="filled" />
-            <TextField id="filled-basic" label="Location" variant="filled" />
+            <Box sx={{
+                display: 'grid',
+                gridTemplateColumns: { sm: '1fr 1fr 1fr' },
+                gap: 2,
+            }}>    
+                <TextField id="filled-basic" label="Search" variant="filled" />
+                <TextField id="filled-basic" label="Classification" variant="filled" />
+                <TextField id="filled-basic" label="Location" variant="filled" />
+            
+            </Box>
         </Grid>
-       
-      
     </Grid>
         )
     }
